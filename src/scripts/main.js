61bc3d99b67702af03a3e853b7aedf0f7c57c32f
Madeline Crowley <madelinecrowley@gmail.com>
2021-04-20 15:08:10 -0500

    sowing the field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,34 +6,12 @@ const yearlyPlan = createPlan()
 
 console.log(yearlyPlan)
 
-import { createAsparagus } from "./seeds/asparagus.js"
-const asparagusSeed = createAsparagus()
-
-import { createCorn } from "./seeds/corn.js"
-const cornSeed = createCorn()
-
-import { createPotato } from "./seeds/potato.js"
-const potatoSeed = createPotato()
-
-import { createSoybean } from "./seeds/soybean.js"
-const soybeanSeed = createSoybean()
-
-import { createSunflower } from "./seeds/sunflower.js"
-const sunflowerSeed = createSunflower()
-
-import { createWheat } from "./seeds/wheat.js"
-const wheatSeed = createWheat()
-
-import { addPlant } from "./field.js"
+import {plantSeeds} from "./tractor.js"
 import {usePlants} from "./field.js"
 
-addPlant(asparagusSeed)
-addPlant(soybeanSeed)
-addPlant(sunflowerSeed)
-addPlant(cornSeed)
-
+plantSeeds(yearlyPlan)
 const plantedField = usePlants()
-console.log(plantedField)
 
+console.log(plantedField)
 
 
---
+++ src/scripts/tractor.js
@@ -0,0 +1,33 @@
+import { createCorn } from "./seeds/corn.js";
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createPotato } from "./seeds/potato.js";
+import { createSoybean } from "./seeds/soybean.js";
+import { createSunflower } from "./seeds/sunflower.js";
+import { createWheat } from "./seeds/wheat.js";
+import {addPlant} from "./field.js"
+
+export const plantSeeds = (plantingPlan) => {
+  for (const row of plantingPlan) {
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === "Corn") {
+        const cornSeed = createCorn();
+        addPlant(cornSeed);
+      } else if (row[i] === "Asparagus") {
+        const asparagusSeed = createAsparagus();
+        addPlant(asparagusSeed);
+      } else if (row[i] === "Potato") {
+        const potatoSeed = createPotato();
+        addPlant(potatoSeed);
+      } else if (row[i] === "Soybean") {
+        const soybeanSeed = createSoybean();
+        addPlant(soybeanSeed);
+      } else if (row[i] === "Sunflower") {
+        const sunflowerSeed = createSunflower();
+        addPlant(sunflowerSeed);
+      } else {
+        const wheatSeed = createWheat();
+        addPlant(wheatSeed);
+      }
+    }
+  }
+};
