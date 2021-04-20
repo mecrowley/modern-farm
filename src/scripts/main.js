d6c340f29f55fb637433612f1d61e3dce3f76df6
Madeline Crowley <madelinecrowley@gmail.com>
2021-04-20 14:12:30 -0500

    tilling the field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,33 @@ const yearlyPlan = createPlan()
 console.log(yearlyPlan)
 
 import { createAsparagus } from "./seeds/asparagus.js"
-
 const asparagusSeed = createAsparagus()
-console.log(asparagusSeed)
 
 import { createCorn } from "./seeds/corn.js"
+const cornSeed = createCorn()
+
 import { createPotato } from "./seeds/potato.js"
+const potatoSeed = createPotato()
+
 import { createSoybean } from "./seeds/soybean.js"
+const soybeanSeed = createSoybean()
+
 import { createSunflower } from "./seeds/sunflower.js"
+const sunflowerSeed = createSunflower()
+
 import { createWheat } from "./seeds/wheat.js"
+const wheatSeed = createWheat()
+
+import { addPlant } from "./field.js"
+import {usePlants} from "./field.js"
+
+addPlant(asparagusSeed)
+addPlant(soybeanSeed)
+addPlant(sunflowerSeed)
+addPlant(cornSeed)
+
+const plantedField = usePlants()
+console.log(plantedField)
+
 
 
